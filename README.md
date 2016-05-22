e6d68c013648aa36b6d9f413066881c3c7c12148
Sebastian Müller <senpo@posteo.de>
2016-05-22 21:32:55 +0200

    Much improvement

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # i3config-macbook
 ## Dependencies
-Needs i3pystatus installed
+Needs i3pystatus installed. Also, the `light` fork from my repos needs to be built and installed from the master and keyboard branch. This enables control of screen and keyboard backlight.
 
-## Installationr
+## Installation
 Put all in your i3 config directory (`~/.i3/` or `~/.config/i3`)
--- status.py
+++ status.py
@@ -21,6 +21,12 @@ status.register("network",
 	interface="wlp3s0b1",
 	format_up="{essid} {quality:03.0f}%",)
 
+status.register("mem",
+	format="RAM:{avail_mem} MB",)
+
+status.register("cpu_usage",
+	format="CPU:{usage:02}%",)
+
 status.register("pulseaudio",
     format="♪{volume}%",)
 
@@ -31,7 +37,7 @@ status.register("backlight",
 
 status.register("backlight",
 	interval=globInterval,
-	format="⌨{percentage}%",
+	format="☐{percentage}%",
 	base_path="/sys/class/leds/smc::kbd_backlight/",)
 
 status.run()
